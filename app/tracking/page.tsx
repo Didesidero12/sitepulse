370e65a94f9d6960a0d101ff2f6276a38bed3444
Joey DiDesidero <joeydidesidero@Jessicas-Air.lan>
2025-12-04 11:29:15 -0800

    update

--- app/tracking/page.tsx
+++ app/tracking/page.tsx
@@ -4,7 +4,7 @@ import TrackingClient from './TrackingClient';
 
 export default function TrackingPage() {
   return (
-    <Suspense fallback={<div className="h-screen bg-gray-900 flex items-center justify-center text-4xl text-white">Loading map...</div>}>
+    <Suspense fallback={<div className="h-screen bg-gray-900 flex items-center justify-center text-6xl text-white">Loading map...</div>}>
       <TrackingClient />
     </Suspense>
   );
